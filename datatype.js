082f1e9a1b908775385c3b50178871a78980e289
Pradosh Adhikari <pradoshak@gmail.com>
2024-07-23 22:57:17 +0545

    day 3

---
+++ datatype.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num;
+num = ["Ram", "Hari", "siya", "Krish"];
+let isValid = true;
+let comp = num[2];
+console.log(comp);
---
+++ datatype.ts
@@ -0,0 +1,9 @@
+import { filter } from "vue/types/umd";
+
+let num: Array<string>;
+num = ["Ram", "Hari", "siya", "Krish"];
+
+let isValid : boolean = true;
+let comp = num[2]
+
+    console.log(comp);
